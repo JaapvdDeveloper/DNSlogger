847cca578306130c8fed46f2184edc0112cdad58
JaapvdDeveloper <Jaapvanduijn166@gmail.com>
2025-04-06 12:05:11 +0200

    file structure DEFINITEVE

--- dns-logger/src/App.java
+++ dns-logger/src/App.java
@@ -1,11 +1,16 @@
 package com.example;
-
+import java.util.logging;
+import java.io;
+import java.net;
 /**
  * Hello world!
  *
  */
 public class DnsLogger
 {
+    Logger logger = DnsLogger.getLogger(DnsLogger.l);
+
+
     public static void main( String[] args )
     {
 
